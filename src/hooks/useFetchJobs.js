24010a6dd19c0bcf4f3a0d69505f7fd63b19a6ab
Omar <elmanssouriomar@gmail.com>
2023-08-11 16:56:36 +0100

    remove comments...

--- src/hooks/useFetchJobs.js
+++ src/hooks/useFetchJobs.js
@@ -1,6 +1,7 @@
 import axios from 'axios'
 import { useEffect, useReducer } from 'react'
 
+// CORS anywhere
 // https://cors-anywhere.herokuapp.com/
 const BASE_URL = 'https://remotive.com/api/remote-jobs?limit=100'
 const ACTIONS = {
@@ -27,27 +28,15 @@ export function useFetchJobs(params) {
     const [state, dispatch] = useReducer(reducer, { loading: true, jobs: [] })
 
     useEffect(() => {
-        // const controller = new AbortController()
-        // const signal = controller.signal
-
         dispatch({ type: ACTIONS.MAKE_REQUEST })
         axios.
             get(BASE_URL, {
-                // signal: signal,
                 params: params
             }).then(res => {
                 dispatch({ type: ACTIONS.GET_DATA, payload: { jobs: res.data.jobs } })
             }).catch(err => {
-                // Check the exception is was caused request cancellation
-                // if (err.name === 'AbortError') { console.error('Request cancelled') }
                 dispatch({ type: ACTIONS.ERROR, payload: { error: err } })
             })
-
-        //cleanup function
-        // return () => {
-        //     // controller.abort()
-        //     source.cancel("axios request cancelled")
-        // }
     }, [params])
 
     return state
